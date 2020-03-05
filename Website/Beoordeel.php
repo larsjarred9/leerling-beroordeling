38cd6f5e565183bf4d5dfbc0e734960dd3a8e5f6
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2020-03-05 10:37:25 +0100

    Update Beoordeel.php

--- Website/Beoordeel.php
+++ Website/Beoordeel.php
@@ -9,7 +9,7 @@
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="GAMECOLLAB.css">
-    <title>Leerling</title>
+    <title>Teach Ground</title>
 </head>
 <body>
 
