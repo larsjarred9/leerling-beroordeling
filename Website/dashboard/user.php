643d4f6bb81fda143aefc7ae6f6a75ac724b5030
Lars Jarred Speetjens <31996561+larsjarred9@users.noreply.github.com>
2020-04-08 17:50:43 +0200

    Removed non working button

--- Website/dashboard/user.php
+++ Website/dashboard/user.php
@@ -393,9 +393,6 @@ $done = $conn->query($sli);
                         <input type="submit" class="btn btn-success" value="Send">
                     </div>
                 </form>
-                <hr>
-                <h5>Quick Actions</h5>
-                <a href="#"><button class="btn btn-primary">Littering</button></a>
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
